c6f0b0e18e3a37c91e40a119c50c27dd6b84dd1b
Daniel-L10N <alejandrolazolemus@gmail.com>
2025-09-19 21:18:48 -0600

    intermedial example

--- 1.1Sintaxis_y_tipos/Array and object/intermedial example.js
+++ 1.1Sintaxis_y_tipos/Array and object/intermedial example.js
@@ -5,14 +5,14 @@ Objetivo: filtrar, transformar y organizar datos en objetos.
 
 👉 Problema:
 Tienes un array de objetos con productos:
-
+*/
 const productos = [
   { nombre: "Laptop", precio: 1200, disponible: true },
   { nombre: "Teléfono", precio: 800, disponible: false },
   { nombre: "Teclado", precio: 100, disponible: true },
   { nombre: "Monitor", precio: 300, disponible: true }
 ];
-
+/*
 
 Filtra solo los productos que están disponibles.
 
@@ -20,3 +20,20 @@ Crea un nuevo array que contenga solo los nombres de los productos.
 
 Calcula el total de los precios de los productos disponibles.
 */
+
+let productosDisponibles = [];
+let productosNoDisponibles =[];
+let productosNoRegistrados =[]
+
+productos.forEach((product) => {
+if (product.disponible === true){
+	productosDisponibles.push(product);
+}else if (product.disponible === false){
+	productosNoDisponibles.push(product);
+}else {
+	productosNoRegustrados.push(product);
+}
+});
+console.log("Los productos disponibles son: ", productosDisponibles.for);
+
+
